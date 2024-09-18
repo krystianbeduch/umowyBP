c81ffa74e3555716c73ca8aae06c1dea5a3123f8
Krystian Będuch <beduch_krystian@o2.pl>
2024-09-18 20:03:20 +0200

    add email template, add logo, add attachments

--- TourOperator/src/main/java/pl/bpwesley/TourOperator/controller/MailController.java
+++ TourOperator/src/main/java/pl/bpwesley/TourOperator/controller/MailController.java
@@ -1,12 +1,19 @@
 package pl.bpwesley.TourOperator.controller;
 
+import jakarta.mail.MessagingException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import pl.bpwesley.TourOperator.service.EmailService;
 
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
 @Controller
+@RequestMapping("/mail")
 public class MailController {
 
     private final EmailService emailService;
@@ -17,13 +24,31 @@ public class MailController {
     }
 
     @ResponseBody
-    @GetMapping("/send-mail")
-    public String sendMail() {
-        emailService.sendSimpleMessage(
+    @GetMapping("/send")
+    public String sendEmailWithReservationConfirmation() throws MessagingException, IOException {
+        // Dane do szablonu mailowego (docelowo DB)
+        Map<String, Object> variables = new HashMap<>();
+        variables.put("client_name", "Imie");
+        variables.put("tour_name", "Wycieczka o nazwie");
+        variables.put("start_date", "data_od");
+        variables.put("end_date", "data_do");
+        variables.put("advance", "zaliczka");
+        variables.put("advance_deadline", "zaliczka termin");
+        variables.put("remaining_amount", "pozostala kwota");
+        variables.put("remaining_amount_deadline", "pozostaala kwota deadline");
+        variables.put("departure_time", "06:00");
+        variables.put("arrival_time", "21:00");
+
+        emailService.sendEmailWithReservationConfirmation(
                 "beduch_krystian@o2.pl",
-                "Test",
-                "cos tam"
+                "Rezerwacja wycieczki ",
+                variables
         );
+//        emailService.sendSimpleMessage(
+//                "beduch_krystian@o2.pl",
+//                "Test",
+//                "cos tam"
+//        );
         return "mail_sent";
     }
 }
--- TourOperator/src/main/java/pl/bpwesley/TourOperator/service/EmailService.java
+++ TourOperator/src/main/java/pl/bpwesley/TourOperator/service/EmailService.java
@@ -1,17 +1,32 @@
 package pl.bpwesley.TourOperator.service;
 
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.InternetAddress;
+import jakarta.mail.internet.MimeMessage;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+
+import java.io.UnsupportedEncodingException;
+import java.util.Map;
 
 @Service
 public class EmailService {
     private final JavaMailSender mailSender;
 
+    private final TemplateEngine templateEngine;
+
+
     @Autowired
-    public EmailService(JavaMailSender mailSender) {
+    public EmailService(JavaMailSender mailSender, TemplateEngine templateEngine) {
         this.mailSender = mailSender;
+        this.templateEngine = templateEngine;
     }
 
     public void sendSimpleMessage(String to, String subject, String text) {
@@ -19,6 +34,42 @@ public class EmailService {
         message.setTo(to);
         message.setSubject(subject);
         message.setText(text);
+        mailSender.send(message);
+    }
+    public void sendEmailWithReservationConfirmation(String to, String subject, Map<String, Object> variables) throws MessagingException, UnsupportedEncodingException {
+        // Utworz i przetworz szablon emaila
+        Context context = new Context();
+        context.setVariables(variables);
+        String body = templateEngine.process("email_templates/reservation_confirmation.html", context);
+
+        // Utworz wiadomosc email
+        MimeMessage message = mailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(message, true, "UTF-8");
+        helper.setFrom(new InternetAddress("beduch.krystian@gmail.com", "Krystian"));
+        helper.setTo(to);
+        helper.setSubject(subject + variables.get("tour_name"));
+        helper.setText(body, true);
+
+        // Dodaj zalaczniki pdf
+        String[] attachments = {
+                "Andrzejki-w-stylu-Country-3545i.pdf",
+                "OWU_BP_Wesley.pdf",
+                "OWU_PZU_NNW.pdf",
+                "Polityka_prywatnosci.pdf",
+                "Regulamin_serwisu.pdf",
+                "Umowa.pdf"
+        };
+
+        for (String fileName : attachments) {
+            ClassPathResource attachment = new ClassPathResource("templates/email_templates/attachments/" + fileName);
+            helper.addAttachment(attachment.getFilename(), attachment);
+        }
+
+        // Zaladuj logo do maila
+        ClassPathResource image = new ClassPathResource("static/images/Logo_Wesley_mini.png");
+        helper.addInline("logoImage", image);
+
+        // Wyslij emaila
         mailSender.send(message);
     }
 }
